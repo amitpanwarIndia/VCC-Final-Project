01ecdb7ffc0c13333edc1665eac5a98426a3a8de
Amit Panwar <amitpanwarit@gmail.com>
2025-04-14 05:32:02 +0000

    Added and Updated files

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.zone
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = "default"
+  subnetwork = "default"
+
+  ip_allocation_policy {}
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "digit-node-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
+
+# This data source will provide our access token for the Kubernetes provider.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "project_id" {}
+variable "region"     { default = "asia-south2" }
+variable "zone"       { default = "asia-south2-a" }
+variable "cluster_name" { default = "digit-cluster" }
